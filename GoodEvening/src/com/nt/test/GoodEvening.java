7edc052be783a0e8e9ceb2aa50918fbb06015670
RajaTL <TL@gmail.com>
2018-06-10 23:34:55 -0700

    Devploper2 to Dev with modify

--- GoodEvening/src/com/nt/test/GoodEvening.java
+++ GoodEvening/src/com/nt/test/GoodEvening.java
@@ -4,6 +4,10 @@ public class GoodEvening {
 
 	public static void main(String[] args) {
 		System.out.println("good evening every body");
+		
+		for(int i=1;i<=10;i++){
+			System.out.println(i);
+		}
 	}
 
 }
